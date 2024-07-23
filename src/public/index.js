5a05b11e0c2450367dfabb0f0582472002e57481
Moniza Oliveira <monapelegrini@gmail.com>
2024-07-23 10:26:54 -0300

    feat: adicionar arquivos de front-end (HTML, JS, CSS) na pasta src/public

---
+++ src/public/index.js
@@ -0,0 +1,9 @@
+function abrirModal() {
+    const modal = document.getElementById('janela-modal');
+    modal.classList.add('abrir');
+}
+
+function fecharModal() {
+    const modal = document.getElementById('janela-modal');
+    modal.classList.remove('abrir');
+}
