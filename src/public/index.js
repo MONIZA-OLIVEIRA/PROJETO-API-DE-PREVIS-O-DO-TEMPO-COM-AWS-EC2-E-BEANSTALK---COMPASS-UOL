5bfb4af3ecef5f01b9efac9aa466824729574755
Moniza Oliveira <monapelegrini@gmail.com>
2024-07-26 09:44:19 -0300

    🎨 feat: lançar nova versão do front-end com estilos e layout atualizados

--- src/public/index.js
+++ src/public/index.js
@@ -1,38 +1,11 @@
-async function consultarTempo() {
-    const cidade = document.getElementById('cidade').value.trim();
+function abrirModal(){
+    const modal = document.getElementById('janela-modal')
+    
+    modal.classList.add('abrir')
 
-    if (cidade === '') {
-        alert('Por favor, digite o nome da cidade.');
-        return;
-    }
-
-    try {
-        const response = await fetch(`/weather?city=${cidade}`);
-        const data = await response.json();
-
-        const modalTitle = document.getElementById('modalTitle');
-        modalTitle.textContent = `Previsão do tempo em ${data.location.name}, ${data.location.country}`;
-
-        const modalContent = document.getElementById('modalContent');
-        modalContent.innerHTML = `
-            <p>Temperatura atual: ${data.current.temperature}°C</p>
-            <p>Condição: ${data.current.weather_descriptions[0]}</p>
-            <p>Última atualização: ${data.current.observation_time}</p>
-        `;
-
-        abrirModal();
-    } catch (error) {
-        console.error('Erro ao buscar dados do clima:', error);
-        alert('Erro ao buscar dados do clima.');
-    }
-}
-
-function abrirModal() {
-    const modal = document.getElementById('janela-modal');
-    modal.classList.add('abrir');
-}
-
-function fecharModal() {
-    const modal = document.getElementById('janela-modal');
-    modal.classList.remove('abrir');
+    modal.addEventListener('click', (e) => {
+        if(e.target.id == 'fechar'|| e.targert.id =='janela-modal'){
+            modal.classList.remove('abrir')    
+        }
+})
 }
